4fdac78070035ccc2234fbe011d2af0d69217e8a
Chouteau Perry <perry.chouteau@epitech.eu>
2025-11-08 19:35:07 +0100

    update from system version

--- includes/geometry.hpp
+++ includes/geometry.hpp
@@ -16,44 +16,6 @@
 #include <cmath>
 #include <limits>
 
-constexpr double epsilon = 10 * std::numeric_limits<double>::epsilon();
-
-/**
- * @brief give the cross product of two vectors
- * 
- * @param a 
- * @param b 
- * @return double 
- */
-inline double cross_product(Vector2f a, Vector2f b) {
-    //cross product formula
-    return a.x * b.y - a.y * b.x;
-}
-
-/**
- * @brief calculate if a point is inside a triangle
- * 
- * @param P 
- * @param A 
- * @param B 
- * @param C 
- * @return true 
- * @return false 
- */
-inline bool is_inside_triangle(Vector2f P, Vector2f A, Vector2f B, Vector2f C) {
-    //To calculate the area of a triangle given three points (x, y), you can use the formula for the area of a triangle formed by three vertices (x1, y1), (x2, y2), and (x3, y3):
-    //Area = 0.5 * |(x1(y2 - y3) + x2(y3 - y1) + x3(y1 - y2))|
-
-    const double abc = 0.5 * abs(A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y));
-
-    const double abp = 0.5 * abs(A.x * (B.y - P.y) + B.x * (P.y - A.y) + P.x * (A.y - B.y));
-    const double bcp = 0.5 * abs(B.x * (C.y - P.y) + C.x * (P.y - B.y) + P.x * (B.y - C.y));
-    const double cap = 0.5 * abs(C.x * (A.y - P.y) + A.x * (P.y - C.y) + P.x * (C.y - A.y));
-
-    //if sum of abp, bcp, cap is equal to abc, then the point is inside the triangle, otherwise it's outside.
-    return std::abs(abc - (abp + bcp + cap)) < epsilon;
-}
-
 /**
  * @brief check if the angle is convex or concave
  * 
@@ -74,12 +36,12 @@ inline bool is_convex(Vector2f A, Vector2f B, Vector2f C, bool clockwise = true)
     Vector2f BA = {A.x - B.x, A.y - B.y};
     Vector2f BC = {C.x - B.x, C.y - B.y};
     //calculate the cross product of BA and BC
-    double cross = cross_product(BA, BC);
+    double cross = BA.cross(BC);
         
     // 1. if it's a clockwise direction, then the cross product should be negative to be a convex angle
     // 2. if it's a counter clockwise direction, then the cross product should be positive to be a convex angle
     // 180 degree in this case is an exception, it's neither convex nor concave, but we can consider it as concave.
-    if (std::abs(cross) < epsilon) 
+    if (std::abs(cross) < epsilon<double>()) 
         return false; // straight line → neither convex nor concave
     else if (clockwise) {
         return cross < 0; // is convex 
--- tests/TestGeometry.cpp
+++ tests/TestGeometry.cpp
@@ -9,25 +9,23 @@ using Vec2 = Vector2f;
 TEST(GeometryTest, CrossProduct) {
     Vec2 a{1.0, 0.0};
     Vec2 b{0.0, 1.0};
-    double result = cross_product(a, b);
+    double result = a.cross(b);
     EXPECT_DOUBLE_EQ(result, 1.0);
 
     Vec2 c{-1.0, 0.0};
-    result = cross_product(c, b);
+    result = c.cross(b);
     EXPECT_DOUBLE_EQ(result, -1.0);
 }
 
 // ----------- point in triangle tests -----------
 
 TEST(GeometryTest, PointInsideTriangle) {
-    Vec2 A{0,0};
-    Vec2 B{1,0};
-    Vec2 C{0,1};
-    Vec2 P_inside{0.25,0.25};
-    Vec2 P_outside{1.0,1.0};
+    Triangle<double> t = {{0,0}, {1, 0}, {0,1}};
+    Vector2f P_inside{0.25,0.25};
+    Vector2f P_outside{1.0,1.0};
 
-    EXPECT_TRUE(is_inside_triangle(P_inside, A, B, C));
-    EXPECT_FALSE(is_inside_triangle(P_outside, A, B, C));
+    EXPECT_TRUE(t.isInside(P_inside));
+    EXPECT_FALSE(t.isInside((P_outside)));
 }
 
 // ----------- convex / concave tests -----------
